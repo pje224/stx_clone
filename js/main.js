da5ccf1eba547335b9c3df87e7ba58a1a570bb15
unknown <pje224@naver.com>
2025-08-25 12:59:44 +0900

    a태그 기본 동작 막기 일단 보류

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 // window를 load한 다음에 fuction 안의 기능을 해라
 window.addEventListener("load", function () {
   //모든 a태그 기본 동작 막기
-  const links = this.document.querySelectorAll("a");
-  links.forEach(function (link) {
-    // console.log(link);
-    link.addEventListener("click", function (event) {
-      event.preventDefault(); // 기본 동작 막기 - 위로가기 방지
-    });
-  });
+  // const links = this.document.querySelectorAll("a");
+  // links.forEach(function (link) {
+  //   // console.log(link);
+  //   link.addEventListener("click", function (event) {
+  //     event.preventDefault(); // 기본 동작 막기 - 위로가기 방지
+  //   });
+  // });
   // document는 html 안의 모든 요소를 뜻함
   //   nav에 마우스 엔터(호버) 했을때
   const nav = document.querySelector(".nav");
